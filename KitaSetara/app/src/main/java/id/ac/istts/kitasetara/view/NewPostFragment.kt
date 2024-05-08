2d415e8905f40b7479cc843d28d0cbb52296069a
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2024-05-09 01:07:24 +0700

    Update NewPostFragment.kt
    Implemented temporary code to get authorName in newPostFragment.kt

--- KitaSetara/app/src/main/java/id/ac/istts/kitasetara/view/NewPostFragment.kt
+++ KitaSetara/app/src/main/java/id/ac/istts/kitasetara/view/NewPostFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import id.ac.istts.kitasetara.R
 import id.ac.istts.kitasetara.databinding.FragmentNewPostBinding
 import id.ac.istts.kitasetara.model.forum.newPost
@@ -27,6 +28,7 @@ class NewPostFragment : Fragment() {
     private lateinit var postBtn: Button
     private val ioScope = CoroutineScope(Dispatchers.IO)
     private val mainScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,13 +41,15 @@ class NewPostFragment : Fragment() {
         postTitle = binding.postTitleEt
         postContent = binding.postContentMT
         postBtn = binding.postButton
-
+        auth = FirebaseAuth.getInstance()
+        val author = auth.currentUser?.displayName.toString() //this returns NULL. Needs to be fixed later -Frans
         postBtn.setOnClickListener {
+            Toast.makeText(context, "Creating post... ${author}", Toast.LENGTH_SHORT).show()
             if(postTitle.text.toString().isNotEmpty() && postContent.text.toString().isNotEmpty()){
                 ioScope.launch {
                     try {
                         //TODO: get author name from user data
-                        val post = newPost(postTitle.text.toString(), postContent.text.toString(), "authorName")
+                        val post = newPost(postTitle.text.toString(), postContent.text.toString(), author)
                         val response = API.retrofitService.createPost(post)
                         withContext(Dispatchers.Main) {
                             Toast.makeText(context, "Post created successfully", Toast.LENGTH_SHORT).show()
