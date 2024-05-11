b7edcea546a67ef22ab374d5fa3e79152a79fb85
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2024-05-11 16:58:41 +0700

    Updated Comment RV in postDetailsFragment

--- KitaSetara/app/src/main/java/id/ac/istts/kitasetara/view/PostDetailsFragment.kt
+++ KitaSetara/app/src/main/java/id/ac/istts/kitasetara/view/PostDetailsFragment.kt
@@ -10,11 +10,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import id.ac.istts.kitasetara.R
+import id.ac.istts.kitasetara.adapters.CommentAdapter
 import id.ac.istts.kitasetara.model.forum.Comment
 import id.ac.istts.kitasetara.model.forum.Post
 import id.ac.istts.kitasetara.viewmodel.DiscussFragmentViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class PostDetailsFragment : Fragment() {
@@ -23,9 +28,10 @@ class PostDetailsFragment : Fragment() {
     private lateinit var authorUsernameTV:TextView
     private lateinit var postTitleTV:TextView
     private lateinit var postContentTV:TextView
-    private lateinit var postCommentsTV:RecyclerView
+    private lateinit var postCommentsRV:RecyclerView
     private lateinit var commentPostEt:EditText
     private lateinit var commentPostBtn:Button
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +44,7 @@ class PostDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val postID = MainActivity.postID
-        //ISSUE -> VIEWMODEL DOES NOT SEND DATA TO THIS FRAGMENT FROM NEWPOSTFRAGMENT. -Frans
-        Log.d("PostDetailsFragment", model.selectedPost.value.toString())
+//        Log.d("PostDetailsFragment", model.selectedPost.value.toString())
         var postTitle = ""
         var postContent = ""
         var postAuthor = ""
@@ -49,7 +54,7 @@ class PostDetailsFragment : Fragment() {
         authorUsernameTV = view.findViewById(R.id.authorUsernameTv)
         postTitleTV = view.findViewById(R.id.postTitleTv)
         postContentTV = view.findViewById(R.id.postContentTv)
-        postCommentsTV = view.findViewById(R.id.postCommentsRV)
+        postCommentsRV = view.findViewById(R.id.postCommentsRV)
         commentPostEt = view.findViewById(R.id.commentPostEt)
         commentPostBtn = view.findViewById(R.id.sendCommentBtn)
         authorUsernameTV.text = postAuthor
@@ -79,9 +84,18 @@ class PostDetailsFragment : Fragment() {
 
         model.comments.observe(viewLifecycleOwner) {
             postComments = it as? ArrayList<Comment> ?: ArrayList()
+            Log.d("PostDetailsFragment", "Comments: ${postComments.toString()}")
+            mainScope.launch {
+                val commentAdapter = CommentAdapter(postComments)
+                postCommentsRV.adapter = commentAdapter
+                commentAdapter.notifyDataSetChanged()
+            }
         }
 
-//        postCommentsTV.adapter = CommentAdapter(postComments)
+
+        postCommentsRV.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.VERTICAL, false)
+
 
         commentPostBtn.setOnClickListener{
 //            val inputComment: String = commentPostEt.text.toString()
